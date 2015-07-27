650b70a94d83aa119067778f21391e61cd625527
vorcil <rajol.kochlashvili@medcorp.co.nz>
2015-07-27 21:53:57 +1200

    update to recursive map flow

--- draw.m
+++ draw.m
@@ -4,8 +4,26 @@
 %Import data from ocsilliscope where $filename is path to datafile and plot;
 filename='ACS137RED.txt';
 data = importdata(filename);
+
+%
 peakfit(data);
 plot(data(:,2));
-p=findpeaks(data(:,2));
 
-MAX
+%Find position of max
+p=findpeaks(data(:,2));
+LMax=max(p);
+LMaxPos=find(data(:,2)==LMax)
+
+%clear data to that peak
+data=data(LMaxPos:size(data(:,2)),:);
+
+%create subset of data 2.5%(arbitrary) of size and locate the minimum index
+tempsize=floor(0.025*size(data(:,2)));
+
+%Find next local minimum and make a subset from that point and the original
+%dataset
+temp=data(1:tempsize(1),2);
+LMin=min(temp);
+LMinPos=find(data(:,2)==LMin)
+
+data=data(LMinPos:size(data(:,2)),:);
